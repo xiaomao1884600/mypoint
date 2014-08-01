54ecae7cd4cd55f4c0d582029dd1e642fdcf2c5f
xiaomao1884600 <wangwu.jun@163.com>
2014-08-01 19:05:28 +0800

    add codeiginer

---
+++ php/codeiginter.php
@@ -0,0 +1,22 @@
+<?php
+/** 
+	PHP CodeIgniter2.1向数据库插入数据时报错Undefined property: User::$db (2012-03-26 17:41:47)转载▼
+标签： php codeigniter userdb it	分类： PHP
+A PHP Error was encountered
+Severity: Notice
+Message: Undefined property: User::$db
+Filename: core/Model.php
+Line Number: 51
+
+解决方法有两种：
+
+第一种：在你的Model类的构造方法中，加入 $this->load->database();   如下：
+     public function MUser(){
+         parent::__construct();
+         $this->load->database();
+     }
+第二种方法：修改application\config\autoload.php文件，如下：
+    将 $autoload['libraries'] = array();
+    改为：  $autoload['libraries'] = array('database');
+
+*/
--- php/download/word.php
+++ php/download/word.php
@@ -3,11 +3,11 @@
 header('Content-Type: text/html;charset=utf-8');
 if (!isset($_GET['file']) || !isset($_GET['type']))
 {
-	print 'no file selsect';
+	print 'no file select';
 	exit();
 }
 $file = $_GET['file'] . '.' . $_GET['type'];
-print_r($file);
+//print_r($file);
 //iconv('UTF-8','GB2312','测试手册.doc')   将字符串按要求的字符编码来转换
 //mb_convert_encoding('测试手册.doc', 'GB2312', 'UTF-8')  转换字符的编码
 //@ 该符号错误控制运算符前缀
